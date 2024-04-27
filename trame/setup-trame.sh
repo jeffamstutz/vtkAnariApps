0f11d1151616b061c97089ca5cb30e5f1c12f1ea
Jefferson Amstutz <jamstutz@nvidia.com>
2024-04-26 22:34:53 -0500

    add trame-tweakpane to setup

--- trame/setup-trame.sh
+++ trame/setup-trame.sh
@@ -1,5 +1,14 @@
 
 python -m venv ~/env/trame
 source ~/env/trame/bin/activate
-pip install setuptools numpy trame trame-client trame-components trame-server trame-vtk trame-vuetify
+pip install \
+  setuptools \
+  numpy \
+  trame \
+  trame-client \
+  trame-components \
+  trame-server \
+  trame-vtk \
+  trame-vuetify \
+  trame-tweakpane
 
