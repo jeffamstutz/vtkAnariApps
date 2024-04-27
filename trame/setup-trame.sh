8456eeb4022b63e1257ce8d633056e5b3fa5684a
Jefferson Amstutz <jamstutz@nvidia.com>
2024-04-26 22:33:09 -0500

    tweaks for Fedora workstation (python 3.12)

--- trame/setup-trame.sh
+++ trame/setup-trame.sh
@@ -1,5 +1,5 @@
 
 python -m venv ~/env/trame
 source ~/env/trame/bin/activate
-pip install numpy trame trame-client trame-components trame-server trame-vtk trame-vuetify
+pip install setuptools numpy trame trame-client trame-components trame-server trame-vtk trame-vuetify
 
